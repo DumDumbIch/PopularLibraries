cb36d88fea971744286bcdf30e82d4d6d42b93e6
DumDumbIch <dumdumbich@gmail.com>
2021-02-28 00:51:29 +0300

    Delete import from MainActivity

--- app/src/main/java/com/dumdumbich/popularlibraries/lesson_1/ui/MainActivity.kt
+++ app/src/main/java/com/dumdumbich/popularlibraries/lesson_1/ui/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.dumdumbich.popularlibraries.lesson_1.ui
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import com.dumdumbich.popularlibraries.databinding.ActivityMainBinding
